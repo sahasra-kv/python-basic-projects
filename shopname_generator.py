a6275e25396bf8ff93df0b9aa5d6c26d3459bc59
sahasra-kv <diyasahasra08@gmail.com>
2025-09-23 07:15:07 +0530

    Update and rename .gitattributes to shopname_generator.py

---
+++ shopname_generator.py
@@ -0,0 +1,5 @@
+#shop name generator
+print("Welcome to shop name generator!")
+related_shop=input("Enter what your shop is about in short(4-8 letters)")
+love_shop=input("Enter what you like the most/describe the shop(can be a person or a thing)")
+print(love_shop+"!"+" "+related_shop)
